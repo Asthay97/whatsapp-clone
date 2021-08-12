8a62d4861e5a99388f90c75881e9e633e6a11538
Astha <yadavastha27@gmail.com>
2021-08-12 13:13:32 +0530

    adding frontend functionalitied

---
+++ whatsapp-clone/src/Sidebar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Sidebar.css';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVerticalIcon from "@material-ui/icons/MoreVert";
+import {Avatar, IconButton} from "@material-ui/core";
+import {SearchOutlined} from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJNaVZy9fqGkE_nv4s_WwtWLIu0qOVS6o8sA&usqp=CAU"/>
+                <div className="sidebar__headerRight">
+                <IconButton>
+                <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                <ChatIcon />
+                </IconButton>
+                <IconButton>
+                <MoreVerticalIcon />
+                </IconButton>
+                
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start a new chat" type = "text" />
+                </div>
+                </div>
+                <div className="sidebar__chats">
+                    <SidebarChat />
+                    <SidebarChat />
+                    <SidebarChat />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDz0xag-jN9pDJJYdAVunKYk3hhS0uamA4",
+    authDomain: "whatsapp-clone-fff24.firebaseapp.com",
+    projectId: "whatsapp-clone-fff24",
+    storageBucket: "whatsapp-clone-fff24.appspot.com",
+    messagingSenderId: "1085486644740",
+    appId: "1:1085486644740:web:f5d0e1e259b9240944ff51",
+    measurementId: "G-M6P10KCJY7"
+  };
