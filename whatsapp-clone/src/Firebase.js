bc3199aa01eaefa5629fc93d6c347c228cdef25d
Astha Yadav <75947851+Asthay97@users.noreply.github.com>
2021-08-19 02:09:03 +0530

    Update Firebase.js

--- whatsapp-clone/src/Firebase.js
+++ whatsapp-clone/src/Firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDz0xag-jN9pDJJYdAVunKYk3hhS0uamA4",
-    authDomain: "whatsapp-clone-fff24.firebaseapp.com",
-    projectId: "whatsapp-clone-fff24",
-    storageBucket: "whatsapp-clone-fff24.appspot.com",
-    messagingSenderId: "1085486644740",
-    appId: "1:1085486644740:web:f5d0e1e259b9240944ff51",
-    measurementId: "G-M6P10KCJY7"
+    <enter your firebase config>
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
